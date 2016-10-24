7f198745ba7c6efaf92315b0cb96ab9a06482afe
Bruce Mbayo <mbayopanda@gmail.com>
2016-10-24 13:20:47 -0700

    Mergin test accounts

--- server/models/data/classe3.sql
+++ server/models/data/classe3.sql
@@ -1,5 +1,5 @@
-INTO `account` (`id`, `type_id`, `enterprise_id`, `number`, `label`, `parent`, `locked`, `cc_id`, `pc_id`, `created`, `classe`, `is_asset`, `reference_id`, `is_brut_link`, `is_title`, `is_charge`) VALUES
-(160, 3, 1, 311, 'MARCHANDISES A', 26, 0, NULL, NULL, '2016-10-23 17:05:34', 3, 1, NULL, NULL, 0, NULL),
-(161, 3, 1, 3111, 'Medicaments', 160, 0, NULL, NULL, '2016-10-23 17:05:34', 3, 1, NULL, NULL, 0, NULL),
-(162, 2, 1, 31110010, 'Medicaments en comprimes', 161, 0, NULL, NULL, '2016-10-23 17:05:34', 3, 1, 15, 1, 0, NULL),
-(163, 2, 1, 31110011, 'Medicaments en Sirop', 161, 0, NULL, NULL, '2016-10-23 17:05:34', 3, 1, 15, 1, 0, NULL);
+INSERT INTO `account` (`id`, `type_id`, `enterprise_id`, `number`, `label`, `parent`, `locked`, `cc_id`, `pc_id`, `created`, `classe`, `is_asset`, `reference_id`, `is_brut_link`, `is_title`, `is_charge`) VALUES
+(160, 3, 1, 311, 'MARCHANDISES A', 26, 0, NULL, NULL, '2016-10-23 17:05:34', NULL, NULL, NULL, NULL, 0, NULL),
+(161, 3, 1, 3111, 'Medicaments', 160, 0, NULL, NULL, '2016-10-23 17:05:34', NULL, NULL, NULL, NULL, 0, NULL),
+(162, 2, 1, 31110010, 'Medicaments en comprimes', 161, 0, NULL, NULL, '2016-10-23 17:05:34', NULL, NULL, NULL, NULL, 0, NULL),
+(163, 2, 1, 31110011, 'Medicaments en Sirop', 161, 0, NULL, NULL, '2016-10-23 17:05:34', NULL, NULL, NULL, NULL, 0, NULL);
